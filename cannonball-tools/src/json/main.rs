a92042b1f16702373ef4e9a9e6d65d8fe66cef39
novafacing <rowanbhart@gmail.com>
2022-10-19 16:48:26 -0400

    Compile for qemu embedding mostly working without qemu embedding (monolithic) mode (for now)

--- cannonball-tools/src/json/main.rs
+++ cannonball-tools/src/json/main.rs
@@ -5,8 +5,12 @@ use futures::stream::StreamExt;
 use log::LevelFilter;
 use memfd_exec::Executable;
 use simple_logger::SimpleLogger;
-use std::fs::Path;
-use tokio::net::{unix::SocketAddr, UnixListener, UnixStream};
+use std::{fs::Path, process::exit, time::Duration};
+use tokio::{
+    net::{unix::SocketAddr, UnixListener, UnixStream},
+    process::Command,
+    time::sleep,
+};
 use tokio_util::codec::Framed;
 use uuid::Uuid;
 
@@ -30,6 +34,19 @@ struct Args {
     args: Vec<String>,
 }
 
+async fn handle(_addr: SocketAddr, stream: UnixStream) {
+    let mut framed = Framed::new(stream, QemuEventCodec {});
+
+    let mut ctr = 0;
+    loop {
+        if let Some(Ok(event)) = framed.next().await {
+            ctr += 1;
+            println!("Received {} events", ctr);
+            println!("Received event: {:?}", event);
+        }
+    }
+}
+
 #[tokio::main]
 fn main() {
     let args = Args::parse();
@@ -48,6 +65,27 @@ fn main() {
         return;
     }
 
+    tokio::spawn(async move {
+        Command::new(args.qemu.unwrap_or_else(|| {
+            if cfg!(feature = "monolithic") {
+                // TODO: This isn't working yet though!
+                "qemu".to_string()
+            } else {
+                error!("No qemu executable provided");
+                exit(1);
+            }
+        }))
+        .arg("-plugin")
+        .arg("../builddir/libcannonball.so,trace_branches=true,trace_syscalls=true,trace_pc=true,trace_reads=true,trace_writes=true,trace_instrs=true")
+        .arg("--")
+        .arg(args.program)
+        .args(args.args)
+        .spawn().expect("QEMU failed to start")
+        .wait().await.expect("QEMU failed to run");
+    });
+
+    sleep(Duration::from_secs(1));
+
     let listener = match UnixListener::bind(sockname) {
         Ok(l) => l,
         Err(e) => {
@@ -55,4 +93,17 @@ fn main() {
             UnixListener::bind(sockname).unwrap()
         }
     };
+
+    loop {
+        match listener.accept().await {
+            Ok((stream, addr)) => {
+                tokio::spawn(async move {
+                    handle(addr, stream).await;
+                });
+            }
+            Err(e) => {
+                error!("Error accepting connection: {}", e);
+            }
+        }
+    }
 }
